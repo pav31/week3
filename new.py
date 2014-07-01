0c7be8b36ddb1bf7f55243963b74f7f9dc27d54f
skver <skver@inbox.ru>
2014-07-02 02:43:46 +0300

    trash

--- new.py
+++ new.py
@@ -1,1 +1,31 @@
 print('dddddddfdfsdfsfsdfddddddddsddd')
+sdasdas
+sdfsdf
+sdfsd
+f
+sdf
+sd
+fsd
+from sdf
+sd
+fsd
+f
+s
+
+s
+df
+sdf
+sd
+fsd
+from sdf
+sd
+fsd
+fs
+f
+setfs
+f
+sdf
+s
+f
+
+sdf
