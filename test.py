e3f876ff27cc68b393393d39e8cade2f579b246e
pav31 <vargan.pg@gmail.com>
2014-07-02 04:57:48 +0300

    4.57

--- test.py
+++ test.py
@@ -6,38 +6,27 @@ print math.factorial(6)
 
 print math.factorial(59) / math.factorial(59 - 6)
 
-num_perms = 1
+num_perm = 1
 for idx in range(59, 59 - 6, -1):
-    num_perms *= idx
-print num_perms
+    num_perm *= idx
+print num_perm
 
-'''
-enum = 10!
-(1,2,3,4,5)
-m = E(0,1,2,3,4,5,6,7,8,9)
-n = 5
-perm = m!/(m-n)!
-comb = m
-'''
 
 print (10 ** 5)
 variants = 100000
 variant = (12.0 / 100000.0)
 print variant
 
-# (0,1,2,3,4,5,6,7,8,9)
-#
-# 10! / (10 - 5)!*10!
 
-print math.factorial(10) / math.factorial(10 - 5)
 # m!/(m-n)!n!
+print math.factorial(10) / math.factorial(10 - 5)
 
-num_perms = 1
+num_perm = 1
 for idx in range(10, 10 - 5, -1):
-    num_perms *= idx
-print num_perms
+    num_perm *= idx
+print num_perm
 
-print 12.0 / num_perms
+print 12.0 / num_perm
 
 # enum = 100000
 # 12 / 100000 = 0.00012
